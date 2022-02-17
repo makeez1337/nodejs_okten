3b603f3c211abd517adfe9abd025e8a2ecc5d09f
makeez1337 <maksymmelnu4yk@gmail.com>
2022-02-17 22:22:12 +0200

    added features

--- app.js
+++ app.js
@@ -10,22 +10,42 @@ const staticPath = path.join(__dirname, 'static');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.set("trust proxy", 1);
 app.use(express.static(staticPath));
 app.set('view engine', '.hbs');
 app.engine('.hbs', engine({defaultLayout: false}));
 app.set('views', staticPath);
 
-const users = [];
+let users = [];
 const usersEmails = [];
 
 app.get('/login', ((req, res) => {
     res.render('login');
 }))
 
+app.get('/signIn', ((req, res) => {
+    res.render('signIn');
+}))
+
+app.post('/signIn', ((req, res) => {
+    const {email, password} = req.body;
+
+    const filteredUser = users.filter(user => {
+        if (user.email.includes(email) && user.password.includes(password)) {
+            return true;
+        }
+    });
+    const userId = filteredUser[0]?.id;
+
+    if (userId) {
+        res.redirect(`/users/${userId}`);
+    } else {
+        res.redirect('/error');
+    }
+}))
+
 app.get('/users', ((req, res) => {
     const {city, age} = req.query;
-    let filteredUsers = [];
+    let filteredUsers = [...users];
 
     if (city && age) {
         filteredUsers = users.filter(user => user.city === city && user.age === age);
@@ -35,19 +55,16 @@ app.get('/users', ((req, res) => {
         filteredUsers = users.filter(user => user.age === age);
     }
 
-    if (filteredUsers.length) {
-        res.json(filteredUsers);
-    } else {
-        res.json(users);
-    }
+    res.json(filteredUsers);
 }))
 
 app.get('/users/:id', ((req, res) => {
     const {id} = req.params;
     const user = users.filter(user => user.id === +id);
+    console.log(user);
 
-    if (user.length && user) {
-        res.json(user);
+    if (user.length) {
+        res.render('user',{user});
     } else {
         res.json('No such user');
     }
@@ -57,9 +74,13 @@ app.get('/error', ((req, res) => {
     res.render('error');
 }))
 
+app.get('*', (req, res) => {
+    res.render('notFound');
+})
+
 app.post('/login', ((req, res) => {
     if (!users.length) {
-        users.push({...req.body, id: 0});
+        users.push({...req.body, id: 1});
         res.redirect('/users');
     } else {
         const lastUserId = users[users.length - 1].id;
@@ -74,12 +95,13 @@ app.post('/login', ((req, res) => {
     usersEmails.push(req.body.email);
 }))
 
-app.use((req, res) => {
-    res.render('notFound');
-})
+app.post('/users/:id',((req, res) => {
+    const {id} = req.params;
+    users = users.filter(user => user.id !== +id);
+    res.redirect('/users');
+}))
+
 
 app.listen(5200, () => {
     console.log('Server on PORT 5200 has started');
 })
-
-
