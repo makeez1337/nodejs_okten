c2c69ee7db079bd95d6ee39e96c54d82fa3c6474
makeez1337 <maksymmelnu4yk@gmail.com>
2022-02-19 18:47:08 +0200

    middleware for loginRouter

--- middleware/isLoginFilled.js
+++ middleware/isLoginFilled.js
@@ -1,5 +1,30 @@
-const isLoginFilled = (req,res,next) => {
+const isLoginFilled = (req, res, next) => {
+    const {email, password, firstName, lastName, age, city} = req.body;
 
+    try {
+        if (!email) {
+            throw new Error('You have to fill email');
+        }
+        if (!password) {
+            throw new Error('You have to fill password');
+        }
+        if (!firstName) {
+            throw new Error('You have to fill firstName');
+        }
+        if (!lastName) {
+            throw new Error('You have to fill lastName');
+        }
+        if (!age) {
+            throw new Error('You have to fill age');
+        }
+        if (!city) {
+            throw new Error('You have to fill city');
+        }
+    } catch (e) {
+        res.status(400).send(e.message);
+    }
+
+    next();
 }
 
 module.exports = isLoginFilled;
--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,10 +1,11 @@
 const {Router} = require('express');
 
 const loginController = require('../controllers/loginController');
+const isLoginFilled = require('../middleware/isLoginFilled');
 
 const loginRouter = Router();
 
 loginRouter.get('/', loginController.renderLoginPage);
-loginRouter.post('/', loginController.createUser);
+loginRouter.post('/', isLoginFilled, loginController.createUser);
 
 module.exports = loginRouter;
