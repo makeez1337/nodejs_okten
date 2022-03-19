31994712c4b48ceec7308c00792ddf0b206a05bd
makeez1337 <maksymmelnu4yk@gmail.com>
2022-03-19 19:19:12 +0200

    implemented new methods/changes in classes

--- src/repositories/token/tokenRepository.ts
+++ src/repositories/token/tokenRepository.ts
@@ -1,13 +1,23 @@
-import { getManager } from 'typeorm';
+import { DeleteResult, getManager } from 'typeorm';
+
 import { IToken, Token } from '../../entity/token';
+import { ITokenDataToSave } from '../../interfaces/token.interface';
 
 class TokenRepository {
-    public async createToken(token:any):Promise<IToken> {
+    public async createToken(token:ITokenDataToSave):Promise<IToken> {
         return getManager().getRepository(Token).save(token);
     }
 
     public async findTokenByUserId(userId:number):Promise<IToken | undefined> {
         return getManager().getRepository(Token).findOne({ userId });
+    }
+
+    public async deleteUserTokenPair(userId: number):Promise<DeleteResult> {
+        return getManager().getRepository(Token).delete({ userId });
+    }
+
+    public async findTokenByParams(params:Partial<IToken>):Promise<IToken | undefined> {
+        return getManager().getRepository(Token).findOne({ ...params });
     }
 }
 
--- src/services/tokenService.ts
+++ src/services/tokenService.ts
@@ -1,29 +1,51 @@
 import jwt from 'jsonwebtoken';
 
+import { DeleteResult } from 'typeorm';
 import { config } from '../config/config';
 import { IToken } from '../entity/token';
 import { tokenRepository } from '../repositories/token/tokenRepository';
+import { ITokenPair, IUserPayload } from '../interfaces/token.interface';
 
 class TokenService {
-    public async generateTokenPair(payload:any)
-        :Promise<{ accessToken:string, refreshToken:string }> {
-        const accessToken = jwt.sign(payload, config.SECRET_ACCESS_KEY as string, { expiresIn: '15m' });
-        const refreshToken = jwt.sign(payload, config.SECRET_REFRESH_KEY as string, { expiresIn: '1d' });
+    public generateTokenPair(payload:IUserPayload):ITokenPair {
+        const accessToken = jwt.sign(
+            payload,
+config.SECRET_ACCESS_KEY as string,
+{ expiresIn: config.EXPIRES_IN_ACCESS },
+        );
+        const refreshToken = jwt.sign(
+            payload,
+config.SECRET_REFRESH_KEY as string,
+{ expiresIn: config.EXPIRES_IN_REFRESH },
+        );
         return {
             accessToken,
             refreshToken,
         };
     }
 
-    public async saveToken(refreshToken:string, userId:number):Promise<IToken> {
+    public async saveToken(refreshToken:string, userId:number, accessToken:string):Promise<IToken> {
         const tokenFromDb = await tokenRepository.findTokenByUserId(userId);
         if (tokenFromDb) {
             tokenFromDb.refreshToken = refreshToken;
+            tokenFromDb.accessToken = accessToken;
             return tokenRepository.createToken(tokenFromDb);
         }
 
-        const token = await tokenRepository.createToken({ refreshToken, userId });
-        return token;
+        return tokenRepository.createToken({ refreshToken, userId, accessToken });
+    }
+
+    public async deleteUserTokenPair(userId:number):Promise<DeleteResult> {
+        return tokenRepository.deleteUserTokenPair(Number(userId));
+    }
+
+    public async verifyToken(authToken: string, tokenType = 'access'): Promise<IUserPayload> {
+        let secretWord = config.SECRET_ACCESS_KEY;
+
+        if (tokenType === 'refresh') {
+            secretWord = config.SECRET_REFRESH_KEY;
+        }
+        return jwt.verify(authToken, secretWord as string) as IUserPayload;
     }
 }
 
