412b3e84d702de514248260cd58d6256354e7357
makeez1337 <maksymmelnu4yk@gmail.com>
2022-04-15 13:57:08 +0300

    upd

--- src/routes/apiRouter.ts
+++ src/routes/apiRouter.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import { userRouter } from './userRouter';
 import { postRouter } from './postRouter';
 import { commentRouter } from './commentRouter';
 import { authRouter } from './authRouter';
 import { studentRouter } from './studentRouter';
+import swaggerDocument from '../docs/swagger.json';
 
 const router = Router();
 
@@ -13,6 +15,7 @@ router.use('/posts', postRouter);
 router.use('/comments', commentRouter);
 router.use('/auth', authRouter);
 router.use('/students', studentRouter);
+router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // @ts-ignore
 router.use('*', (err, req, res, next) => {
     res
